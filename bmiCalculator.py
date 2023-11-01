6cad1083e9f80e422d788603a3a55ce829f6405e
Bernardo Rodriguez <70609136+brod760@users.noreply.github.com>
2023-11-01 13:57:38 -0700

    Add files via upload

---
+++ bmiCalculator.py
@@ -0,0 +1,12 @@
+#This is a simple bmi calculator. 
+#It takes user inputs such as height in meters and weight in kilograms
+
+print("Please enter your height in meters!")
+height = input() # stored as str 
+print("Please enter weight in kg!")
+weight = input() # stored as str
+int_weight = int(weight) 
+float_height = float(height) #you have to use the float to be able to maths 
+bmi = int_weight / (float_height **2) # ** rasies the ^2
+finalbmi = round(bmi)  # round function gives the nearest whole number 
+print(f"Your bmi is {finalbmi}")
