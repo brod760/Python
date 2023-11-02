0c95bb6210a6063c9364fa3352bb85d9733a975d
Bernardo Rodriguez <70609136+brod760@users.noreply.github.com>
2023-11-02 12:47:59 -0700

    Add files via upload

---
+++ tip calcu.py
@@ -0,0 +1,11 @@
+print("Welcome to the Tip Calculator")
+bill = float(input("What was the total of the bill? ")) #input is saved as str
+Numberof_ppl = int(input("How many people will split the bill?" )) #input is saved as str
+Percent_ofTip = float(input("How much of tip do you want to give? 10, 12, or 15?" )) # input is saved as str
+tip_as_percent = Percent_ofTip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+x = round(total_bill, 2)
+print(f"Each persone will pay ${x}")
+
+
