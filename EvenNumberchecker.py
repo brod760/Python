24d33eaba4ca11284dbcd94d72db1fa8db2bc1de
Bernardo Rodriguez <70609136+brod760@users.noreply.github.com>
2023-11-03 13:09:17 -0700

    Add files via upload

---
+++ EvenNumberchecker.py
@@ -0,0 +1,9 @@
+#is takes user input and checks if number is even or odd#
+#with the use of if else statment#
+number = int(input("What number do you want checked?" ))
+if number % 2 == 0: #checks for remainders#
+    print("This is an even number!")
+    print("Thank you for using!")
+else:
+    print("This is an odd number!")
+    print("Thank you for using!")
